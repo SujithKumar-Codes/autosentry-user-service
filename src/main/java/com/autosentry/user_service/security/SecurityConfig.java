1207e8c4bc21d6e52df228faa49e3975838602b5
Sujithkumar <kumarsujith21911@gmail.com>
2026-05-15 18:38:32 +0530

    feat: Add JWT filter class to intercept incoming requests and verify JWT

--- src/main/java/com/autosentry/user_service/security/SecurityConfig.java
+++ src/main/java/com/autosentry/user_service/security/SecurityConfig.java
@@ -10,15 +10,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 // import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // You will inject your JWT filter here later
-    // private final JwtAuthenticationFilter jwtAuthFilter;
-    // public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) { this.jwtAuthFilter = jwtAuthFilter; }
+     private final JwtAuthenticationFilter jwtAuthFilter;
+     public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) { this.jwtAuthFilter = jwtAuthFilter; }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
-        // http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
